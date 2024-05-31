0e707ab5ae52550ac07d33637fd6bb371f14b5f2
Jeremy Chone <jeremy.chone@gmail.com>
2024-05-31 09:10:09 -0700

    + HasSeaFields - add 'sea_apply_select_columns(sea_select)'

--- src/field/sea/has_sea_fields.rs
+++ src/field/sea/has_sea_fields.rs
@@ -1,7 +1,7 @@
-use crate::field::SeaFields;
-use sea_query::{ColumnRef, DynIden, IntoIden};
+use crate::field::{HasFields, SeaFields};
+use sea_query::{ColumnRef, DynIden, IntoIden, SelectStatement};
 
-pub trait HasSeaFields {
+pub trait HasSeaFields: HasFields {
 	/// Returns the `Fields` containing the `Field` items that have non-`None` values.
 	fn not_none_sea_fields(self) -> SeaFields;
 
@@ -12,8 +12,17 @@ pub trait HasSeaFields {
 	fn sea_idens() -> Vec<DynIden>;
 
 	/// Returns the list of column refs (takes the eventual #[field(rel = "table_name")])
+	/// WARNING: This won't have the aliases if there need to be some.
+	///          Use `sea_apply_select_columns(select)` or `T::field_metas()` to build manually.
+	/// TODO: Need to use the `field_metas().. meta.sea_column_ref()``
 	fn sea_column_refs() -> Vec<ColumnRef>;
 
 	/// Returns the list of column refs with the given relation (e.g., table name) and IntoIden (.e.g., StringIden or SIden)
 	fn sea_column_refs_with_rel(rel: impl IntoIden) -> Vec<ColumnRef>;
+
+	fn sea_apply_select_columns(&self, sea_select: &mut SelectStatement) {
+		for meta in Self::field_metas().iter() {
+			meta.sea_apply_select_column(sea_select);
+		}
+	}
 }
