9ca1f431435fa00b9154915198e69d8d899e7ce6
Jeremy Chone <jeremy.chone@gmail.com>
2025-07-02 12:18:03 -0700

    ~ sqlite - derive - add HasSqliteFields

--- src/field/sqlite/has_sqlite_fields.rs
+++ src/field/sqlite/has_sqlite_fields.rs
@@ -1,14 +1,11 @@
-use crate::field::{HasFields, SqliteFields};
+use crate::field::{HasFields, SqliteColumnRef, SqliteFields};
 
 pub trait HasSqliteFields: HasFields {
 	/// Returns the `Fields` containing the `Field` items that have non-`None` values.
-	fn not_none_lite_fields(self) -> SqliteFields;
+	fn sqlite_not_none_fields(self) -> SqliteFields;
 
 	/// Returns the `Fields` containing all of the `Field`.
-	fn all_lite_fields(self) -> SqliteFields;
-
-	// /// Return the sea_query::DynIden for each field (just matching the field name)
-	// fn sea_idens() -> Vec<DynIden>;
+	fn sqlite_all_fields(self) -> SqliteFields;
 
 	// /// Returns the list of column refs (takes the eventual #[field(rel = "table_name")])
 	// /// WARNING: This won't have the aliases if there need to be some.
@@ -16,12 +13,6 @@ pub trait HasSqliteFields: HasFields {
 	// /// TODO: Need to use the `field_metas().. meta.sea_column_ref()``
 	// fn lite_column_refs() -> Vec<&'static str>;
 
-	// /// Returns the list of column refs with the given relation (e.g., table name) and IntoIden (.e.g., StringIden or SIden)
-	// fn sea_column_refs_with_rel(rel: impl IntoIden) -> Vec<ColumnRef>;
-
-	// fn sea_apply_select_columns(&self, sea_select: &mut SelectStatement) {
-	// 	for meta in Self::field_metas().iter() {
-	// 		meta.sea_apply_select_column(sea_select);
-	// 	}
-	// }
+	/// Returns the list of column refs with the given relation (e.g., table name) and IntoIden (.e.g., StringIden or SIden)
+	fn sqlite_column_refs_with_rel(rel: &str) -> Vec<SqliteColumnRef>;
 }
